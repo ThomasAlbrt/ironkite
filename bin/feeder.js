e3f598a646012c63a9a473d1b5fe84c349893451
Thomas <thomas.alibert14@gmail.com>
2019-11-22 10:20:40 +0100

    test.js added

--- bin/feeder.js
+++ bin/feeder.js
@@ -3,27 +3,30 @@ const spotModel = require("../models/Spot");
 const data = require("../public/js/scraping");
 
 async function feedDb() {
-    const apiRes = await data()
-
-    apiRes.forEach(spot => {
-        spotModel
-            .findOneAndUpdate({
-                spotName: spot.spotName,
-                wind: spot.wind,
-                region: spot.region,
-                url: spot.url
-            }, {
-                expire: new Date()
-            }, {
-                upsert: true,
-                new: true,
-                setDefaultsOnInsert: true
-            })
-            .then(dbRes => console.log(dbRes))
-            .catch(err => {
-                console.log(err)
-            });
-    })
+    try {
+        const apiRes = await data()
+        apiRes.forEach(spot => {
+            spotModel
+                .findOneAndUpdate({
+                    spotName: spot.spotName,
+                    wind: spot.wind,
+                    region: spot.region,
+                    url: spot.url
+                }, {
+                    expire: new Date()
+                }, {
+                    upsert: true,
+                    new: true,
+                    setDefaultsOnInsert: true
+                })
+                .then(dbRes => console.log(dbRes))
+                .catch(err => {
+                    console.log(err)
+                });
+        })
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 feedDb();
